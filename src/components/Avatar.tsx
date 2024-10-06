5ae313fb9e82cf2f89090ae2a35fe766880d8748
DleiaDev <markowebdeveloper@gmail.com>
2024-10-06 12:30:37 +0200

    refactor: Avatar.tsx to support Next.js <Image /> component

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,14 +1,22 @@
+import Image, { ImageProps } from "next/image";
 import { Avatar as AvatarMain, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { cn } from "@/lib/utils";
 
-type Props = {
+export type Props = {
   className?: string;
+  imageProps: Omit<ImageProps, "src"> & {
+    src?: string;
+  };
 };
 
-export default function Avatar({ className }: Props) {
+export default function Avatar({ className, imageProps }: Props) {
   return (
     <AvatarMain className={cn(className)}>
-      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+      <AvatarImage asChild src={imageProps.src}>
+        {imageProps.src && (
+          <Image {...imageProps} src={imageProps.src} alt={imageProps.alt} />
+        )}
+      </AvatarImage>
       <AvatarFallback>MI</AvatarFallback>
     </AvatarMain>
   );
