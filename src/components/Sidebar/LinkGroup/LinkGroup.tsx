8fa84959f30e1e09ef9da0830cd3c7d9bb8c4176
DleiaDev <markowebdeveloper@gmail.com>
2024-10-14 12:31:05 +0200

    refactor: Sidebar.tsx

--- src/components/Sidebar/LinkGroup/LinkGroup.tsx
+++ src/components/Sidebar/LinkGroup/LinkGroup.tsx
@@ -1,10 +1,10 @@
-import React, { ReactElement, ReactNode } from "react";
+import React from "react";
 import Link from "../Link/Link";
 import styles from "./LinkGroup.module.scss";
 
 type Props = {
   title?: string | false;
-  links: ReactElement<HTMLUListElement>;
+  links: React.ComponentProps<typeof Link>[];
   className?: string;
 };
 
@@ -12,7 +12,13 @@ export default function LinkGroup({ title, links, className }: Props) {
   return (
     <div className={className}>
       {title && <span className={styles["link-group__title"]}>{title}</span>}
-      {links}
+      <ul>
+        <li>
+          {links.map((link, index) => (
+            <Link key={link.svg.name} {...link} className="my-2" />
+          ))}
+        </li>
+      </ul>
     </div>
   );
 }
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,7 +2,6 @@
 
 import Link from "./Link/Link";
 import LinkGroup from "./LinkGroup/LinkGroup";
-import styles from "./Sidebar.module.scss";
 import HospitalIcon from "@/icons/hospital.svg";
 import LogoSvg from "@/icons/logo.svg";
 import { cn } from "@/lib/utils";
@@ -10,6 +9,7 @@ import Button from "../Button";
 import { ChevronRightIcon, ChevronLeftIcon } from "@radix-ui/react-icons";
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
+import Separator from "@/components/ui/separator";
 
 type Props = {
   className?: string;
@@ -53,7 +53,7 @@ export default function Sidebar({ className }: Props) {
         </Button>
 
         <nav className="h-svh overflow-auto flex flex-col">
-          <div className={styles["sidebar__container"]}>
+          <div className="p-4">
             {/* Logo */}
             <div className="flex gap-3 items-center px-4 h-16">
               <LogoSvg className="w-8 h-8" />
@@ -75,141 +75,109 @@ export default function Sidebar({ className }: Props) {
               )}
             </div>
 
-            <ul>
-              <li>
-                <Link
-                  className={styles["sidebar__dashboard-link"]}
-                  title={!collapsed && "Dashboard"}
-                  href="/dashboard"
-                  svg={{ name: "dashboard" }}
-                />
-              </li>
-            </ul>
-
-            {/* Clinic group */}
             <LinkGroup
-              className={styles["sidebar__link-group"]}
+              className="mb-6"
+              links={[
+                {
+                  title: !collapsed && "Dashboard",
+                  href: "/dashboard",
+                  svg: { name: "dashboard" },
+                },
+              ]}
+            />
+
+            <LinkGroup
+              className="mb-6"
               title={!collapsed && "CLINIC"}
-              links={
-                <ul>
-                  <li>
-                    <Link
-                      title={!collapsed && "Reservations"}
-                      href="/reservations"
-                      svg={{
-                        type: "gray",
-                        name: "calendar-check",
-                      }}
-                    />
-                  </li>
-                  <li>
-                    <Link
-                      title={!collapsed && "Patients"}
-                      href="/patients"
-                      svg={{ name: "user" }}
-                    />
-                  </li>
-                  <li>
-                    <Link
-                      title={!collapsed && "Treatments"}
-                      href="/treatments"
-                      svg={{ name: "stethoscope" }}
-                    />
-                  </li>
-                  <li>
-                    <Link
-                      title={!collapsed && "Staff"}
-                      href="/staff"
-                      svg={{ name: "users" }}
-                    />
-                  </li>
-                </ul>
-              }
+              links={[
+                {
+                  title: !collapsed && "Reservartions",
+                  href: "/reservations",
+                  svg: {
+                    type: "gray",
+                    name: "calendar-check",
+                  },
+                },
+                {
+                  title: !collapsed && "Patients",
+                  href: "/patients",
+                  svg: { name: "user" },
+                },
+                {
+                  title: !collapsed && "Treatments",
+                  href: "/treatments",
+                  svg: { name: "stethoscope" },
+                },
+                {
+                  title: !collapsed && "Staff",
+                  href: "/staff",
+                  svg: { name: "users" },
+                },
+              ]}
             />
 
-            {/* Finance group */}
             <LinkGroup
-              className={styles["sidebar__link-group"]}
+              className="mb-6"
               title={!collapsed && "FINANCE"}
-              links={
-                <ul>
-                  <li>
-                    <Link
-                      title={!collapsed && "Accounts"}
-                      href="/accounts"
-                      svg={{ name: "money" }}
-                    />
-                  </li>
-                  <li>
-                    <Link
-                      title={!collapsed && "Sales"}
-                      href="/sales"
-                      svg={{ name: "chart" }}
-                    />
-                  </li>
-                  <li>
-                    <Link
-                      title={!collapsed && "Purchases"}
-                      href="/purchases"
-                      svg={{ name: "invoice" }}
-                    />
-                  </li>
-                  <li>
-                    <Link
-                      title={!collapsed && "Payment methods"}
-                      href="/payment-methods"
-                      svg={{ name: "credit-card" }}
-                    />
-                  </li>
-                </ul>
-              }
+              links={[
+                {
+                  title: !collapsed && "Accounts",
+                  href: "/accounts",
+                  svg: { name: "money" },
+                },
+                {
+                  title: !collapsed && "Sales",
+                  href: "/sales",
+                  svg: { name: "chart" },
+                },
+                {
+                  title: !collapsed && "Purchases",
+                  href: "/purchases",
+                  svg: { name: "invoice" },
+                },
+                {
+                  title: !collapsed && "Payment Methods",
+                  href: "/payment-methods",
+                  svg: { name: "credit-card" },
+                },
+              ]}
             />
 
-            {/* Physical assets */}
             <LinkGroup
-              className={`${styles["sidebar__link-group"]} ${styles["sidebar__link-group--physical"]}`}
+              className="mb-2"
               title={!collapsed && "PHYSICAL ASSETS"}
-              links={
-                <ul>
-                  <li>
-                    <Link
-                      title={!collapsed && "Stock"}
-                      href="/stock"
-                      svg={{ name: "bottle" }}
-                    />
-                  </li>
-                  <li>
-                    <Link
-                      title={!collapsed && "Peripherals"}
-                      href="/peripherals"
-                      svg={{ name: "hospital-bed" }}
-                    />
-                  </li>
-                </ul>
-              }
+              links={[
+                {
+                  title: !collapsed && "Stock",
+                  href: "/stock",
+                  svg: { name: "bottle" },
+                },
+                {
+                  title: !collapsed && "Peripherals",
+                  href: "/peripherals",
+                  svg: { name: "hospital-bed" },
+                },
+              ]}
             />
           </div>
 
-          {/* Divider */}
-          <div className={styles["sidebar__link-divider"]}></div>
+          <Separator />
 
-          <div className={styles["sidebar__container"]}>
-            <ul>
-              <li>
-                <Link
-                  title={!collapsed && "Report"}
-                  href="/report"
-                  svg={{ name: "doughnut-chart" }}
-                />
-              </li>
-              <li>
-                <Link
-                  title={!collapsed && "Customer Support"}
-                  href="/customer-support"
-                  svg={{ name: "customer-support" }}
-                />
-              </li>
-            </ul>
+          <div className="p-4">
+            <LinkGroup
+              links={[
+                {
+                  title: !collapsed && "Reports",
+                  href: "/report",
+                  svg: { name: "doughnut-chart" },
+                },
+                {
+                  title: !collapsed && "Customer Support",
+                  href: "/customer-support",
+                  svg: { name: "customer-support" },
+                },
+              ]}
+            />
           </div>
         </nav>
       </div>
