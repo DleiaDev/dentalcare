9f0d785d5ef07528039fe960435aac5d7e00a56b
DleiaDev <markowebdeveloper@gmail.com>
2024-11-10 16:26:26 +0100

    add: working day creation schema

--- src/zod/WorkingDay.ts
+++ src/zod/WorkingDay.ts
@@ -1,7 +1,13 @@
 import { z } from "zod";
 import { Employee, EmployeeSchema } from "./Employee";
-import { TimeSlot, TimeSlotSchema } from "./TimeSlot";
+import {
+  CreateTimeSlotsInputSchema,
+  TimeSlot,
+  TimeSlotSchema,
+} from "./TimeSlot";
 import { DayOfWeek, DayOfWeekEnum } from "./utils/dayOfWeek";
+import { ClinicSettings } from "./ClinicSettings";
+import { formatTime } from "@/lib/utils";
 
 // ------- Model -------
 
@@ -10,7 +16,7 @@ export type WorkingDay = {
   employeeId: string;
   Employee: Employee;
   dayOfWeek: DayOfWeek;
-  timeSlots: TimeSlot[];
+  TimeSlots: TimeSlot[];
   createdAt: Date;
   updatedAt: Date;
 };
@@ -20,19 +26,61 @@ export const WorkingDaySchema: z.ZodType<WorkingDay> = z.object({
   employeeId: z.string().uuid(),
   Employee: z.lazy(() => EmployeeSchema),
   dayOfWeek: DayOfWeekEnum,
-  timeSlots: z.array(TimeSlotSchema),
+  TimeSlots: z.array(TimeSlotSchema),
   createdAt: z.date(),
   updatedAt: z.date(),
 });
 
 // ------- Input -------
-
-export type CreateWorkingDayInput = Omit<
-  WorkingDay,
-  "id" | "createdAt" | "updatedAt" | "Employee" | "timeSlots"
+export type CreateWorkingDayInput = z.infer<
+  ReturnType<typeof CreateWorkingDayInputSchema>
 >;
+export const CreateWorkingDayInputSchema = (clinicSettings: ClinicSettings) => {
+  const dayToStartAndEndTime = clinicSettings.WorkingDays.reduce(
+    (acc, workingDay) => {
+      const startTime = workingDay.TimeSlots[0].startTime;
+      const endTime =
+        workingDay.TimeSlots[workingDay.TimeSlots.length - 1].endTime;
+      acc[workingDay.dayOfWeek] = { startTime, endTime };
+      return acc;
+    },
+    {} as Record<
+      DayOfWeek,
+      { startTime: TimeSlot["startTime"]; endTime: TimeSlot["endTime"] }
+    >,
+  );
 
-export const CreateWorkingDayInputSchema = z.object({
-  employeeId: z.string().uuid(),
-  dayOfWeek: DayOfWeekEnum,
-});
+  return z.object({
+    WorkingDays: z
+      .object({
+        dayOfWeek: DayOfWeekEnum,
+        TimeSlots: CreateTimeSlotsInputSchema,
+      })
+      .superRefine((workingDay, ctx) => {
+        const { startTime: clinicStartTime, endTime: clinicEndTime } =
+          dayToStartAndEndTime[workingDay.dayOfWeek];
+        const { startTime: employeeStartTime } = workingDay.TimeSlots[0];
+        const { endTime: employeeEndTime } =
+          workingDay.TimeSlots[workingDay.TimeSlots.length - 1];
+        if (employeeStartTime < clinicStartTime) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: `Start time can't be earlier then clinic's start time (${formatTime(clinicSettings.timeFormat, clinicStartTime)})`,
+            path: ["TimeSlots", 0, "startTime"],
+            fatal: true,
+          });
+          return z.NEVER;
+        }
+        if (clinicEndTime < employeeEndTime) {
+          ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: `End time can't be later then clinic's end time (${formatTime(clinicSettings.timeFormat, clinicEndTime)})`,
+            path: ["TimeSlots", workingDay.TimeSlots.length - 1, "endTime"],
+            fatal: true,
+          });
+          return z.NEVER;
+        }
+      })
+      .array(),
+  });
+};
