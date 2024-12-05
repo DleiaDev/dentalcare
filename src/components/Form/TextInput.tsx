24dd483d708b6cbeeb71dc6db26977fb3f63cdd7
DleiaDev <markowebdeveloper@gmail.com>
2024-12-05 12:33:24 +0100

    feat(test input): allow text input to be controlled

--- src/components/Form/TextInput.tsx
+++ src/components/Form/TextInput.tsx
@@ -1,50 +1,110 @@
+import get from "lodash.get";
+import { ChangeEvent } from "react";
+import { Controller, useFormContext } from "react-hook-form";
 import { cn } from "@/lib/utils";
-import { forwardRef, InputHTMLAttributes } from "react";
 import Label from "./Label";
 import ErrorMessage from "./ErrorMessage";
-import { useFormContext } from "react-hook-form";
 
-type Props = InputHTMLAttributes<HTMLInputElement> & {
-  name: string;
+type Props = {
+  name?: string;
+  value?: string;
   label?: string;
+  className?: string;
   containerClassName?: string;
+  autoFocus?: boolean;
+  onValueChange?: (value: string) => void;
 };
 
-const TextInput = forwardRef<HTMLInputElement, Props>(
-  ({ className, name, label, containerClassName, ...props }, ref) => {
-    const {
-      register,
-      formState: { errors },
-    } = useFormContext();
-    const field = register(name);
-    const errorMessage = errors[name]?.message;
+function UIComponent({
+  className,
+  name,
+  label,
+  containerClassName,
+  errorMessage,
+  value,
+  onValueChange,
+  ...props
+}: Props & {
+  onValueChange: Exclude<Props["onValueChange"], undefined>;
+  errorMessage?: string;
+}) {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onValueChange(e.target.value);
+  };
 
-    return (
-      <div className={cn("mb-7", containerClassName)}>
-        {label && <Label htmlFor={name}>{label}</Label>}
-        <input
-          id={name}
-          type="text"
-          className={cn(
-            "flex h-12 w-full rounded-md border border-border bg-transparent px-3 py-1 font-medium shadow-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50",
-            errorMessage
-              ? "border-error ring-error/20"
-              : "focus:border-primary ring-primary/20 ",
-            className,
-          )}
-          {...props}
-          {...field}
-          ref={(element) => {
-            field.ref(element);
-          }}
-        />
-        {typeof errorMessage === "string" && (
-          <ErrorMessage>{errorMessage}</ErrorMessage>
+  return (
+    <div className={cn("mb-7", containerClassName)}>
+      {label && <Label htmlFor={name}>{label}</Label>}
+      <input
+        {...props}
+        value={value}
+        id={name}
+        type="text"
+        className={cn(
+          "flex h-12 w-full rounded-md border border-border bg-transparent px-3 py-1 font-medium shadow-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50",
+          errorMessage
+            ? "border-error ring-error/20"
+            : "focus:border-primary ring-primary/20 ",
+          className,
         )}
-      </div>
-    );
-  },
-);
-TextInput.displayName = "TextInput";
+        onChange={handleChange}
+      />
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </div>
+  );
+}
 
-export default TextInput;
+function FormWrapper({
+  name,
+  onValueChange,
+  ...props
+}: Props & { name: string }) {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+  const errorMessage = get(errors, name)?.message;
+  const handleValueChange = (
+    value: string,
+    onFieldChange: Exclude<Props["onValueChange"], undefined>,
+  ) => {
+    onFieldChange(value);
+    if (onValueChange) onValueChange(value);
+  };
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <UIComponent
+          {...props}
+          name={name}
+          value={field.value}
+          onValueChange={(value) => handleValueChange(value, field.onChange)}
+          errorMessage={
+            typeof errorMessage === "string" ? errorMessage : undefined
+          }
+        />
+      )}
+    />
+  );
+}
+
+export default function TextInput({
+  name,
+  value,
+  onValueChange,
+  ...props
+}: Props) {
+  if (name !== undefined && value === undefined)
+    return <FormWrapper {...props} name={name} onValueChange={onValueChange} />;
+  if (
+    name === undefined &&
+    typeof value === "string" &&
+    onValueChange !== undefined
+  )
+    return (
+      <UIComponent {...props} value={value} onValueChange={onValueChange} />
+    );
+}
