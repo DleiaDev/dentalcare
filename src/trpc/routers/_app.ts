e14e405ed0b4c1b07fc9fe131c8154d1c57107cd
DleiaDev <markowebdeveloper@gmail.com>
2024-12-11 10:28:53 +0100

    feat(trpc): add peripherals

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { holidaysRouter } from "./holidays";
+import { peripheralsRouter } from "./peripherals";
 import { treatmentsRouter } from "./treatments";
 
 export const appRouter = router({
   holidays: holidaysRouter,
   treatments: treatmentsRouter,
+  peripherals: peripheralsRouter,
 });
 
 export type AppRouter = typeof appRouter;
