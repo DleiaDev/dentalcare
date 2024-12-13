329d82a89ce9351f220c619cf0dc884319bbe708
DleiaDev <markowebdeveloper@gmail.com>
2024-12-13 17:51:40 +0100

    refactor(multiselect): remove forwardRef, add type keyword to imports, reference value as Option["value"]

--- src/components/Form/MultiSelect.tsx
+++ src/components/Form/MultiSelect.tsx
@@ -5,10 +5,10 @@ import {
 } from "@/components/ui/popover";
 import get from "lodash.get";
 import {
-  ButtonHTMLAttributes,
-  ComponentType,
-  forwardRef,
-  ReactNode,
+  type ButtonHTMLAttributes,
+  type ComponentType,
+  type ReactNode,
+  type RefObject,
   useState,
 } from "react";
 import { Controller, useFormContext } from "react-hook-form";
@@ -44,8 +44,9 @@ type Group = {
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   name?: string;
-  value?: string[];
-  onValueChange?: (value: string[]) => void;
+  value?: Option["value"][];
+  onValueChange?: (value: Option["value"][]) => void;
+  ref?: RefObject<HTMLButtonElement>;
   options?: Option[];
   groups?: Group[];
   label?: string;
@@ -76,7 +77,7 @@ function Options({
   options,
   onSelect,
 }: {
-  value: string[];
+  value: Option["value"][];
   options: Option[];
   onSelect: (v: string) => void;
 }) {
@@ -111,183 +112,179 @@ type UIComponentProps = Props & {
   onValueChange: Exclude<Props["onValueChange"], undefined>;
 };
 
-const UIComponent = forwardRef<HTMLButtonElement, UIComponentProps>(
-  (
-    {
-      name,
-      value,
-      errorMessage,
-      label,
-      modal,
-      placeholder,
-      groups,
-      options,
-      className,
-      containerClassName,
-      isFetching,
-      fetchingFailed,
-      onValueChange,
-      onFirstOpen,
-      ...props
-      // selectedValueFormat,
-    },
-    ref,
-  ) => {
-    const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-    const [firstPopoverOpen, setIsFirstPopoverOpen] = useState(false);
+function UIComponent({
+  name,
+  value,
+  errorMessage,
+  label,
+  ref,
+  modal,
+  placeholder,
+  groups,
+  options,
+  className,
+  containerClassName,
+  isFetching,
+  fetchingFailed,
+  onValueChange,
+  onFirstOpen,
+  ...props
+  // selectedValueFormat,
+}: UIComponentProps) {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [firstPopoverOpen, setIsFirstPopoverOpen] = useState(false);
 
-    const handleTogglePopover = () => {
-      setIsFirstPopoverOpen(true);
-      setIsPopoverOpen((prev) => !prev);
-      if (!firstPopoverOpen && onFirstOpen) onFirstOpen();
-    };
+  const handleTogglePopover = () => {
+    setIsFirstPopoverOpen(true);
+    setIsPopoverOpen((prev) => !prev);
+    if (!firstPopoverOpen && onFirstOpen) onFirstOpen();
+  };
 
-    const handleOptionSelect = (optValue: string) => {
-      const newValue = value.includes(optValue)
-        ? value.filter((v) => v !== optValue)
-        : [...value, optValue];
-      onValueChange(newValue);
-    };
+  const handleOptionSelect = (optValue: string) => {
+    const newValue = value.includes(optValue)
+      ? value.filter((v) => v !== optValue)
+      : [...value, optValue];
+    onValueChange(newValue);
+  };
 
-    return (
-      <div className={cn("mb-7", containerClassName)}>
-        {label && <Label htmlFor={name}>{label}</Label>}
-        <Popover
-          open={isPopoverOpen}
-          onOpenChange={setIsPopoverOpen}
-          modal={modal}
-        >
-          <PopoverTrigger asChild>
-            <Button
-              {...props}
-              intent="text"
-              color="black"
-              ref={ref}
-              onClick={handleTogglePopover}
-              className={cn(
-                "flex flex-wrap justify-start gap-2 min-h-12 w-full rounded-md border border-border bg-transparent px-3 py-2 font-medium shadow-sm transition-colors select-none max-w-full",
-                errorMessage
-                  ? "border-error ring-error/20"
-                  : fetchingFailed
-                    ? "border-error bg-input-invalid"
-                    : "",
-                className,
-              )}
-              disabled={fetchingFailed}
-            >
-              {isFetching ? (
-                <div className="text-ellipsis text-muted-foreground">
-                  Loading...
-                </div>
-              ) : fetchingFailed ? (
-                <div className="text-ellipsis">
-                  An error occurred during loading
-                </div>
-              ) : value.length === 0 && placeholder ? (
-                <span
-                  className={cn(
-                    "text-muted-foreground",
-                    errorMessage && "text-error/70",
-                  )}
-                >
-                  {placeholder}
-                </span>
-              ) : (
-                value.map((v) => {
-                  const option = getOptionByValue(v, options, groups);
-                  const IconComponent = option?.icon;
-                  return (
-                    <Badge key={v} color="accent" className="text-base">
-                      {IconComponent && (
-                        <IconComponent className="h-5 w-5 mr-2 text-gray-700" />
-                      )}
-                      {option?.label}
-                    </Badge>
-                  );
-                })
-              )}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0 relative" align="start">
-            {isFetching ? (
-              <div className="h-48 flex items-center justify-center">
-                <Spinner className="text-primary w-10 h-10" />
-              </div>
-            ) : (
-              <Command className="animate-in fade-in duration-300">
-                <CommandInput placeholder="Search..." />
-                <CommandList>
-                  <CommandEmpty>No results found.</CommandEmpty>
-                  {options ? (
-                    <Options
-                      value={value}
-                      options={options}
-                      onSelect={handleOptionSelect}
-                    />
-                  ) : groups ? (
-                    groups.map((group) => (
-                      <CommandGroup key={group.label}>
-                        <Options
-                          value={value}
-                          options={group.options}
-                          onSelect={handleOptionSelect}
-                        />
-                      </CommandGroup>
-                    ))
-                  ) : null}
-                  <CommandGroup></CommandGroup>
-                </CommandList>
-              </Command>
+  return (
+    <div className={cn("mb-7", containerClassName)}>
+      {label && <Label htmlFor={name}>{label}</Label>}
+      <Popover
+        open={isPopoverOpen}
+        onOpenChange={setIsPopoverOpen}
+        modal={modal}
+      >
+        <PopoverTrigger asChild>
+          <Button
+            {...props}
+            intent="text"
+            color="black"
+            ref={ref}
+            onClick={handleTogglePopover}
+            className={cn(
+              "flex flex-wrap justify-start gap-2 min-h-12 w-full rounded-md border border-border bg-transparent px-3 py-2 font-medium shadow-sm transition-colors select-none max-w-full",
+              errorMessage
+                ? "border-error ring-error/20"
+                : fetchingFailed
+                  ? "border-error bg-input-invalid"
+                  : "",
+              className,
             )}
-          </PopoverContent>
-        </Popover>
-        {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
-      </div>
-    );
-  },
-);
+            disabled={fetchingFailed}
+          >
+            {isFetching ? (
+              <div className="text-ellipsis text-muted-foreground">
+                Loading...
+              </div>
+            ) : fetchingFailed ? (
+              <div className="text-ellipsis">
+                An error occurred during loading
+              </div>
+            ) : value.length === 0 && placeholder ? (
+              <span
+                className={cn(
+                  "text-muted-foreground",
+                  errorMessage && "text-error/70",
+                )}
+              >
+                {placeholder}
+              </span>
+            ) : (
+              value.map((v) => {
+                const option = getOptionByValue(v, options, groups);
+                const IconComponent = option?.icon;
+                return (
+                  <Badge key={v} color="accent" className="text-base">
+                    {IconComponent && (
+                      <IconComponent className="h-5 w-5 mr-2 text-gray-700" />
+                    )}
+                    {option?.label}
+                  </Badge>
+                );
+              })
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0 relative" align="start">
+          {isFetching ? (
+            <div className="h-48 flex items-center justify-center">
+              <Spinner className="text-primary w-10 h-10" />
+            </div>
+          ) : (
+            <Command className="animate-in fade-in duration-300">
+              <CommandInput placeholder="Search..." />
+              <CommandList>
+                <CommandEmpty>No results found.</CommandEmpty>
+                {options ? (
+                  <Options
+                    value={value}
+                    options={options}
+                    onSelect={handleOptionSelect}
+                  />
+                ) : groups ? (
+                  groups.map((group) => (
+                    <CommandGroup key={group.label}>
+                      <Options
+                        value={value}
+                        options={group.options}
+                        onSelect={handleOptionSelect}
+                      />
+                    </CommandGroup>
+                  ))
+                ) : null}
+                <CommandGroup></CommandGroup>
+              </CommandList>
+            </Command>
+          )}
+        </PopoverContent>
+      </Popover>
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </div>
+  );
+}
 UIComponent.displayName = "UIComponent";
 
-const FormWrapper = forwardRef<HTMLButtonElement, Props & { name: string }>(
-  ({ name, onValueChange, ...props }, ref) => {
-    const {
-      control,
-      formState: { errors },
-    } = useFormContext();
+function FormWrapper({
+  name,
+  onValueChange,
+  ...props
+}: Props & { name: string }) {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
 
-    const errorMessage = get(errors, name)?.message;
+  const errorMessage = get(errors, name)?.message;
 
-    const handleValueChange = (
-      value: Option["value"][],
-      onFieldChange: (value: Option["value"][]) => void,
-    ) => {
-      onFieldChange(value);
-      if (onValueChange) onValueChange(value);
-    };
+  const handleValueChange = (
+    value: Option["value"][],
+    onFieldChange: (value: Option["value"][]) => void,
+  ) => {
+    onFieldChange(value);
+    if (onValueChange) onValueChange(value);
+  };
 
-    return (
-      <Controller
-        name={name}
-        control={control}
-        render={({ field }) => (
-          <UIComponent
-            {...props}
-            ref={ref}
-            name={name}
-            value={field.value}
-            onValueChange={(value) => handleValueChange(value, field.onChange)}
-            errorMessage={
-              typeof errorMessage === "string" ? errorMessage : undefined
-            }
-          />
-        )}
-      />
-    );
-  },
-);
-FormWrapper.displayName = "FormWrapper";
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <UIComponent
+          {...props}
+          name={name}
+          value={field.value}
+          onValueChange={(value) => handleValueChange(value, field.onChange)}
+          errorMessage={
+            typeof errorMessage === "string" ? errorMessage : undefined
+          }
+        />
+      )}
+    />
+  );
+}
 
-const MultiSelect = forwardRef<HTMLButtonElement, Props>((props, ref) => {
+export default function MultiSelect(props: Props) {
   const { name, value, onValueChange } = props;
   if (name !== undefined && value === undefined) {
     return <FormWrapper {...props} name={name} onValueChange={onValueChange} />;
@@ -297,19 +294,11 @@ const MultiSelect = forwardRef<HTMLButtonElement, Props>((props, ref) => {
     onValueChange !== undefined
   ) {
     return (
-      <UIComponent
-        {...props}
-        ref={ref}
-        value={value}
-        onValueChange={onValueChange}
-      />
+      <UIComponent {...props} value={value} onValueChange={onValueChange} />
     );
   } else {
     throw new Error(
       "Supply either a 'name' or ('value' and 'onValueChange') props to <MultiSelect />",
     );
   }
-});
-MultiSelect.displayName = "MultiSelect";
-
-export default MultiSelect;
+}
