eb2d75465d7b2a204400c3d3decc05006a47248c
DleiaDev <markowebdeveloper@gmail.com>
2025-01-15 23:56:31 +0100

    refactor(ui): Navbar.tsx to support regex in paths

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,27 +7,27 @@ import Dropdown from "@/components/Dropdown";
 import { ChevronDownIcon } from "@radix-ui/react-icons";
 import ProfileCard from "@/components/ProfileCard";
 
-const pageNameMap = {
-  "/dashboard": "Dashboard",
-  "/reservations": "Reservations",
-  "/staff": "Staff",
-  "/treatments": "Treatments",
-  "/stocks": "Stocks",
-  "/peripherals": "Peripherals",
-  "/peripherals/new": "Peripherals",
-} as const;
-
-type PagePath = keyof typeof pageNameMap;
+const pageNameMap = [
+  { regex: "/dashboard", title: "Dashboard" },
+  { regex: "/reservations", title: "Reservations" },
+  { regex: "/staff", title: "Staff" },
+  { regex: "/treatments", title: "Treatments" },
+  { regex: "/stocks", title: "Stocks" },
+  { regex: "/peripherals/new", title: "Peripherals" },
+  { regex: "/peripherals/[a-zA-Z]+", title: "Peripherals" },
+  { regex: "/peripherals", title: "Peripherals" },
+] as const;
 
 export default function Navbar() {
   const pathname = usePathname();
-  if (!(pathname in pageNameMap)) return null;
-  const pagePath = pathname as PagePath;
-  const pageName = pageNameMap[pagePath];
+  const pageTitle = pageNameMap.find(
+    (page) => pathname.match(page.regex) !== null,
+  )?.title;
+  if (!pageTitle) return null;
 
   return (
     <div className="h-[5rem] px-8 flex justify-between items-center border-b border-b-border">
-      <h2 className="text-3xl font-bold">{pageName}</h2>
+      <h2 className="text-3xl font-bold">{pageTitle}</h2>
 
       <div className="flex items-center gap-x-8">
         <Search />
