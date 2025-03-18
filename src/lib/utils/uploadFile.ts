cd55b0a66c8f1302d727e183b9d9227c3e117e8c
DleiaDev <markowebdeveloper@gmail.com>
2025-03-18 13:52:51 +0100

    refactor: move cloudinary config into a util file

--- src/lib/utils/uploadFile.ts
+++ src/lib/utils/uploadFile.ts
@@ -1,13 +1,8 @@
 "use server";
 
-import { v2 as cloudinary, UploadApiOptions } from "cloudinary";
+import { UploadApiOptions } from "cloudinary";
+import cloudinary from "../cloudinary";
 import fileToBase64 from "./fileToBase64";
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 type Options = {
   folder: Exclude<UploadApiOptions["folder"], undefined>;
